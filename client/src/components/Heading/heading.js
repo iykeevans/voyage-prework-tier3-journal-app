2f94acdb8f15d2c7636b9bea78606ccbe85a0d2d
iykeevans <elochi238@gmail.com>
2019-08-26 15:53:31 +0100

    chore: add readme

--- client/src/components/Heading/heading.js
+++ client/src/components/Heading/heading.js
@@ -4,11 +4,12 @@ import moment from 'moment';
 import './heading.scss';
 
 function Heading({ toggleForm, authenticated, allJournals, heading }) {
+  console.log(allJournals);
   return (
     <div className="heading">
       <h1>{ heading || 'All Journals' } 
         <span>
-          updated { allJournals && moment(allJournals[0].created_at).fromNow()}
+          updated { allJournals && allJournals.length && moment(allJournals[0].created_at).fromNow()}
         </span>
       </h1>
       {authenticated && <button onClick={ toggleForm }>Add new</button>}
