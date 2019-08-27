6cf1138c119bdae7141d16c01b4871c4bd12262e
iykeevans <elochi238@gmail.com>
2019-08-27 21:24:29 +0100

    changed endpoint

--- client/src/services/client.js
+++ client/src/services/client.js
@@ -5,7 +5,7 @@ const prodUrl = 'https://chingu-journal-app.herokuapp.com/api/v1';
 const API_BASE_URL = 'http://localhost:5000/api/v1';
 
 export default axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: prodUrl,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${getToken()}`
